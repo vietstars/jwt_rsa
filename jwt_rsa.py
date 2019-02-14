87b66410aafc60aee371e10d7065d59ad9a49ba9
Việt Stars <vietstar.nt@gmail.com>
2019-02-14 22:59:24 +0700

    Update jwt_rsa.py

--- jwt_rsa.py
+++ jwt_rsa.py
@@ -1,4 +1,4 @@
-# import rsa
+import rsa
 
 # (pubkey, privkey) = rsa.newkeys(512)
 
@@ -21,24 +21,37 @@
 
 # response = rsa.decrypt(token, privkey)
 # print('msg: ',response)
-import jwt
-import time
+# import jwt
+# import time
 
-payload = {'user':'vietstar', 'header':'jsontoken', 'exp': time.time() + 3600}
+# payload = {'user':'vietstar', 'header':'jsontoken', 'exp': time.time() + 3600}
 
-privkey = open('private.key').read()
-pubkey = open('public.key').read()
+# privkey = open('private.key').read()
+# pubkey = open('public.key').read()
 
-public = 'MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGWuHs+PnLBOThh8PCbz37YwJ+/dTM90hcGTHidtKU0Q3VAB4wCwoWyjxYncjX4tSEuenHmPAP63vaEAMkmR9xkzHINhDDAH5bVah0rTbRjOE4QhZ9fgwlJ7t1tkdDiD+jm6qfGQ8GyzM5NPfOV5h43LOW1jMFAWzoGiwUZlzochAgMBAAE='
+# public = 'MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGWuHs+PnLBOThh8PCbz37YwJ+/dTM90hcGTHidtKU0Q3VAB4wCwoWyjxYncjX4tSEuenHmPAP63vaEAMkmR9xkzHINhDDAH5bVah0rTbRjOE4QhZ9fgwlJ7t1tkdDiD+jm6qfGQ8GyzM5NPfOV5h43LOW1jMFAWzoGiwUZlzochAgMBAAE='
 
-# token = jwt.encode(payload, privkey , algorithm='RS256').decode('utf-8')
+# # token = jwt.encode(payload, privkey , algorithm='RS256').decode('utf-8')
+# # print('token: ',token)
+
+# # msg = jwt.decode(token, pubkey , algorithms=['RS256'])
+# # print('msg: ',msg)
+
+# token = jwt.encode(payload, public , algorithm='HS256').decode('utf-8')
 # print('token: ',token)
 
-# msg = jwt.decode(token, pubkey , algorithms=['RS256'])
+# msg = jwt.decode(token, public , algorithms=['HS256'])
 # print('msg: ',msg)
 
-token = jwt.encode(payload, public , algorithm='HS256').decode('utf-8')
-print('token: ',token)
+from Crypto.PublicKey import RSA
 
-msg = jwt.decode(token, public , algorithms=['HS256'])
-print('msg: ',msg)
+key = RSA.generate(1024)
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+print(private_key)
+
+public_key = key.publickey().export_key()
+file_out = open("public.pem", "wb")
+file_out.write(public_key)
+print(public_key)
